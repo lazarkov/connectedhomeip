9050669bdf5792bae2414c2b1bd7b73ba6dc0d2e
Lazar Kovacic <lkovacic@amazon.com>
2021-06-17 15:01:36 +0200

    NO-JIRA: Adding helper scripts

--- test-chip.sh
+++ test-chip.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
+adb push chip_server_kvs.tmp /tmp/chip_server_kvs.tmp
+
+echo "***************************************"
+echo "** Now CHIP pair with your TV        **"
+echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool pairing onnetwork 0 20202021 3840 10.78.130.161 11097
+sleep 5
+
 echo "***************************************"
 echo "** Now CHIP will turn on off your TV **"
 echo "***************************************"
-
-./out/debug/standalone/chip-tool onoff off 1
 sleep 5
-./out/debug/standalone/chip-tool onoff on 1
+./out/debug/standalone/chip-tool onoff toggle 1
+sleep 5
+./out/debug/standalone/chip-tool onoff toggle 1
+sleep 5
 
 echo "***************************************"
 echo "** Now CHIP will mute unmute your TV **"
 echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool onoff toggle 2
+sleep 5
+./out/debug/standalone/chip-tool onoff toggle 2
 
-./out/debug/standalone/chip-tool onoff off 2
-sleep 2
-./out/debug/standalone/chip-tool onoff on 2
+sleep 5
+echo "***************************************"
+echo "** Now CHIP will set TV to low power **"
+echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool lowpower sleep 1
+
+echo "***************************************"
+echo "** Now CHIP will turn on     your TV **"
+echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool onoff toggle 1
+
+echo "***************************************"
+echo "** Now CHIP will volume up TV        **"
+echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool levelcontrol step 0 2 0 0 0 2
+
+echo "***************************************"
+echo "** Now CHIP will volume down TV        **"
+echo "***************************************"
+sleep 5
+./out/debug/standalone/chip-tool levelcontrol step 1 2 0 0 0 2
 
 sleep 5
 echo "***************************************"
